7a13f62775722b68def49aeab6be9f69426bca76
Wilson Cheng <34016903+wilsonchia@users.noreply.github.com>
2018-11-20 20:20:13 +0800

    2018-11-20 Wilson Cheng AccountDetail Model function Update

--- Models/AccountDetailModels.cs
+++ Models/AccountDetailModels.cs
@@ -54,16 +54,98 @@ namespace MvcDemand.Models
             , "@AccJobNo" , "@AccMobile", "@AccPhone", "@AccEmail",  "@AccPassword"
             , "@AccNotation", "@AccImage", "@AccDateS", "@AccDateE", "@AccStatus" };
 
-
-        public DataTable returnDataTable()
-        {
+        public DataTable returnDataTable() {
             funDataTable.Clear(); funQuerySQL = ""; funDicParas = null;
-            funQuerySQL = "select a.*, sa.SystemTitle as nameDeptNo, sb.SystemTitle as nameJobNo, sb.SystemTitle as nameClass" 
+            funQuerySQL = "select a.*, sa.SystemTitle as titleDeptNo, sb.SystemTitle as titleJobNo, sb.SystemTitle as titleClass" 
                         + " from AccountDetail a inner join SystemDataDetail sa on sa.systemclass='AccDeptNo' and sa.SystemValue=a.AccDeptNo "
                         + " inner join SystemDataDetail sb on sb.SystemClass='AccJobNo' and sb.SystemValue=a.AccJobNo "
-                        + " inner join SystemDataDetail sc on sc.SystemClass='AccClass' and sc.SystemValue = a.AccClass";
+                        + " inner join SystemDataDetail sc on sc.SystemClass='AccClass' and sc.SystemValue = a.AccClass Where 1=1 ";
             funDataTable = dbClass.msDataTableToDataBase(funQuerySQL, funDicParas);
             return funDataTable;
+        }
+
+        public List<oAccountDetail> listObjAccountDetail() {
+            List<oAccountDetail> rtnList = new List<oAccountDetail>();
+            funDataTable = null; funDataTable = returnDataTable();
+            foreach (DataRow dr in funDataTable.Rows)
+            {
+                oAccountDetail item = new oAccountDetail();
+                item.oAccIndex = dr["AccIndex"].ToString();
+                item.oAccNo = dr["AccNo"].ToString();
+                item.oAccName = dr["AccName"].ToString();
+                item.oAccClass = dr["AccClass"].ToString();
+                item.oAccDeptNo = dr["AccDeptNo"].ToString();
+                item.oAccJobNo = dr["AccJobNo"].ToString();
+                item.oAccMobile = dr["AccMobile"].ToString();
+                item.oAccPhone = dr["AccPhone"].ToString();
+                item.oAccEmail = dr["AccEmail"].ToString();
+                item.oAccPassword = dr["AccPassword"].ToString();
+                item.oAccNotation = dr["AccNotation"].ToString();
+                item.oAccImage = dr["AccImage"].ToString();
+                item.oAccDateS = dr["AccDateS"].ToString();
+                item.oAccDateE = dr["AccDateE"].ToString();
+                item.oAccStatus = dr["AccStatus"].ToString();
+                item.oTitleAccDeptNo = dr["titleDeptNo"].ToString();
+                item.oTitleAccJobNo = dr["titleJobNo"].ToString();
+                item.oTitleAccClass = dr["titleClass"].ToString();
+                item.oTitleAccStatus = (dr["AccStatus"].ToString() == "O") ? "啟用" : "停用";
+            }
+            return rtnList;
+        }
+
+        public List<oAccountDetail> detailObjAccountDetail(string funAccIndex, string funAccNo)
+        {
+            List<oAccountDetail> rtnList = new List<oAccountDetail>();
+            rtnList = listObjAccountDetail().Where(x => x.oAccIndex == funAccIndex && x.oAccNo == funAccNo).ToList();
+            return rtnList;
+        }
+
+        public string DataCreate(List<object> listDataCreate)
+        {
+            funExecuteValue = ""; funExecuteSQL = ""; funDicParas = null;
+            try {
+                funDicParas = dbClass.GetListToNewDictionary(aryDeclareName, listDataCreate);
+                funExecuteSQL = "Insert Into AccountDetail Values ( ";
+                for (int i = 0; i < aryDeclareName.Count; i++) { funExecuteSQL += string.Format(@"{0}{1}", aryDeclareName[i], (i < aryDeclareName.Count - 1) ? "," : ""); }
+                funExecuteSQL += " ) ";
+                funExecuteValue = dbClass.msExecuteValueToDataBase(funExecuteSQL, funDicParas);
+            } catch (Exception ex) {
+                funExecuteValue = ex.Message;
+            }
+            return funExecuteValue;
+        }
+
+        public string DataUpdate(List<object> listDataUpdate)
+        {
+            funExecuteSQL = ""; funExecuteValue = ""; funDicParas = null;
+            try {
+                funDicParas = dbClass.GetListToNewDictionary(aryDeclareName, listDataUpdate);
+                funExecuteSQL = "Update AccountDetail Set ";
+                for (int i = 2; i < aryColumnName.Count; i++) {
+                    funExecuteSQL += string.Format(@" {0}={1}{2} ", aryColumnName[i], aryDeclareName[i], (i < aryColumnName.Count - 1) ? "," : "");
+                }
+                funExecuteSQL += " Where ";
+                for (int i = 0; i < 2; i++) {
+                    funExecuteSQL += string.Format(@" {0}={1}{2}", aryColumnName[i], aryDeclareName[i], (i < 1) ? " and " : "");
+                }
+                funExecuteValue = dbClass.msExecuteValueToDataBase(funExecuteSQL, funDicParas);
+            } catch (Exception ex) {
+                funExecuteValue = ex.Message;
+            }
+            return funExecuteValue;
+        }
+
+        public string DataDelete(List<object> listDataDelete) {
+            funExecuteValue = ""; funExecuteSQL = ""; funDicParas = null; List<string> aryDeclareValue = new List<string>();
+            try {
+                aryDeclareValue = new List<string>() { "@AccIndex", "@AccNo" };
+                funDicParas = dbClass.GetListToNewDictionary(aryDeclareValue, listDataDelete);
+                funExecuteSQL = @"Delete from AccountDetail Where AccIndex=@AccIndex and AccNo=@AccNo ";
+                funExecuteValue = dbClass.msExecuteValueToDataBase(funExecuteSQL, funDicParas);
+            } catch (Exception ex) {
+                funExecuteValue = ex.Message;
+            }
+            return funExecuteValue;
         }
 
     }
