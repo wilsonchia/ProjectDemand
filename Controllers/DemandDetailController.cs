65ff89669ea873135fb50ce2cf91a97b967229fb
Wilson Cheng <34016903+wilsonchia@users.noreply.github.com>
2018-12-17 20:53:25 +0800

    2018-12-17 wilson cheng DemandDetail Data Create Function

--- Controllers/DemandDetailController.cs
+++ Controllers/DemandDetailController.cs
@@ -12,7 +12,8 @@ namespace MvcDemand.Controllers
 
         SystemDataDetailModels sdModel = new SystemDataDetailModels();
         DemandDetailModels ddModel = new DemandDetailModels();
-
+        AccountDetailModels adModel = new AccountDetailModels();
+        AccountRelationModels arModel = new AccountRelationModels();
 
         //
         // GET: /DemandDetail/
@@ -35,6 +36,45 @@ namespace MvcDemand.Controllers
             viewModel.selAccountDetailMan = sdModel.selObjSystemDataDetail("", "請選擇", "");
             ViewBag.selAccountDetailMan = viewModel.selAccountDetailMan;
             return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public RedirectResult Create(FormCollection form)
+        {
+
+
+            return Redirect("");
+        }
+
+        public string returnValueToAccIndex(string fAccIndex)
+        {
+            string returnValue = ""; string oAccDeptNo = "";
+            List<oAccountDetail> delList = adModel.listObjAccountDetail().Where(x => x.oAccIndex == fAccIndex).ToList();
+            oAccDeptNo = (delList.Count > 0) ? delList[0].oAccDeptNo.ToString() : "";
+            List<oAccountRelation> ardListB = arModel.listAccountRelation().Where(x => x.oAccDeptNo == oAccDeptNo && x.oRelationClass == "B").ToList();
+            List<oAccountRelation> ardListC = arModel.listAccountRelation().Where(x => x.oAccDeptNo == oAccDeptNo && x.oRelationClass == "C").ToList();
+            List<oAccountRelation> ardListD = arModel.listAccountRelation().Where(x => x.oAccDeptNo == oAccDeptNo && x.oRelationClass == "E").ToList();            
+            returnValue = oAccDeptNo;
+            if (ardListB.Count > 0) {
+                returnValue += "^";
+                foreach (oAccountRelation itemA in ardListB) {
+                    returnValue += string.Format(@"{0}_{1}_{2}{3}", itemA.oAccIndex, itemA.oAccNo, itemA.oAccName, "|");
+                }
+            } else { returnValue += "^Zero"; }
+            if (ardListC.Count > 0) {
+                returnValue += "^";
+                foreach (oAccountRelation itemB in ardListC) {
+                    returnValue += string.Format(@"{0}_{1}_{2}{3}", itemB.oAccIndex, itemB.oAccNo, itemB.oAccName, "|");
+                }
+            } else { returnValue += "^Zero"; }
+            if (ardListD.Count > 0) {
+                returnValue += "^";
+                foreach (oAccountRelation itemC in ardListD) {
+                    returnValue += string.Format(@"{0}_{1}_{2}{3}", itemC.oAccIndex, itemC.oAccNo, itemC.oAccName, "|");
+                }
+            } else { returnValue += "^Zero"; }
+            return returnValue;
         }
 
     }
